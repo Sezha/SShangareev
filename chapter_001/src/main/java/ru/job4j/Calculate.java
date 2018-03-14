fc3fbb97899bfe5f266936567d7f2b3be1c288f2
Sezha <sezhaekb@gmail.com>
2018-03-14 23:45:38 +0500

    1.4. JUnit[#45100]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -16,4 +16,17 @@ public class Calculate {
 	public static void main (String[] args) {
 		System.out.println("Hello World!!");
 	}
+
+
+	/**
+	* Method echo 
+	* Вывод строки в консоль со словом Echo
+	* @param name Your name
+	* @return Echo plus Your name
+	*/
+	public String echo (String name) {
+		return "Echo, Echo, Echo : " + name;
+	}
+
+
 }

3b723f2b154ffe6bf6e66ab271e121858f5454b3
Sezha <sezhaekb@gmail.com>
2018-03-14 23:05:55 +0500

    1.3. JavaDoc. Code convention[#45091]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+* Class Calculate
+* 
+* @author Sergei Shangareev 
+* @version 1.0
+*/
 public class Calculate {
+	
+	/**
+	* main 
+	* Констурктор вывод строки в консоль.
+	* @param args - args
+	*/
 	public static void main (String[] args) {
 		System.out.println("Hello World!!");
 	}

796ba44c0f3ae96a29d8e9f0e3946aa4d4c0b721
Sezha <sezhaekb@gmail.com>
2018-03-20 10:25:32 +0500

    4.3. Вычисление площади треугольника исправленный метод  exist

--- Chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ Chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -66,12 +66,9 @@ public class Triangle {
 	* @return
 	*/
 	private boolean exist(double ab, double ac, double bc) {
-		 if ((ab + ac > bc) && (ab + bc > ac) && (ac + bc > ab)) {
-		 	return true;
+		return ((ab + ac > bc) && (ab + bc > ac) && (ac + bc > ab));
 		 }
 
-	   	return false;
-	}
 
 
 }

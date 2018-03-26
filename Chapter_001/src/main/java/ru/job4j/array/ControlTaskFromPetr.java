d142b37ae2c747c5960c973ff34caaf27145561c
Sezha <sezhaekb@gmail.com>
2018-03-26 23:26:26 +0500

    6.6. Удаление дубликатов в массиве.[#46528]

---
+++ Chapter_001/src/main/java/ru/job4j/array/ControlTaskFromPetr.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+/**
+ * class ControlTaskFromPetr.
+ * This  class should sort new array from two equivalent arrays by ascending.
+ * @author Sergei Shangareev (sezhaekb@gmail.com).
+ * @version  1.0
+ */
+public class ControlTaskFromPetr {
+
+    public int[] sortArray(int[] first, int[] second) {
+        int size = first.length + second.length;
+        int[] array = new int[size];
+        for (int i = 0; i < size/2; i++) {
+            if (first[i] > second[i]) {
+                array[i + i] = second[i];
+                array[i + i + 1] = first[i];
+            } else {
+                array[i + i] = first[i];
+                array[i + i + 1] = second[i];
+            }
+        }
+        for (int index = size-1; index > 0; index--){
+            for (int i = 0; i < index; i++) {
+                    if (array[i] > array[i + 1]) {
+                        int temp = array[i];
+                        array[i] = array [i + 1];
+                        array[i + 1] = temp;
+                    }
+            }
+        }
+        return array;
+
+    }
+}
---
+++ Chapter_001/src/test/java/ru/job4j/array/ControlTaskFromPetrTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test of class ControlTaskFromPetr
+ * This test class should check new array from two equivalent arrays by ascending.
+ * @author Sergei Shangareev (sezhaekb@gmail.com).
+ * @version  1.0
+ */
+public class ControlTaskFromPetrTest {
+
+    @Test
+    public void setArrayOneWithThreeAndArrayTwoWithFore() {
+        ControlTaskFromPetr  testArray = new ControlTaskFromPetr();
+        int [] firstArray = {1, 3};
+        int[] secondArray = {2,4};
+        int[] result = testArray.sortArray(firstArray, secondArray);
+        int [] expected = {1, 2, 3, 4};
+        assertThat(result, is(expected));
+    }
+}

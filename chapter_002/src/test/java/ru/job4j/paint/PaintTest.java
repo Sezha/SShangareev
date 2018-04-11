a9b79e24c27516f96106c61ca164b831d75a6e02
Sezha <sezhaekb@gmail.com>
2018-04-11 20:54:17 +0500

    4.5. Рефакторинг теста @Before @After[#49172]

--- chapter_002/src/test/java/ru/job4j/paint/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/paint/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.paint;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -13,20 +15,25 @@ import static org.junit.Assert.*;
  * @version 1.0.
  */
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+
+    }
 
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфер для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в память для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишущие в консоль.
-        new Paint().draw(new Square());
-        // проверяем результат вычисления
-        assertThat(
-                new String(out.toByteArray()),
+       new Paint().draw(new Square());
+       assertThat(
+                new String(this.out.toByteArray()),
                 is(
                         new StringBuilder()
                                 .append("+ + + +")
@@ -37,7 +44,24 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("*    ")
+                                .append("**   ")
+                                .append("***  ")
+                                .append("**** ")
+                                .append("*****")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
         System.setOut(stdout);
     }
 }
